a35622a835be55119c498164202d8b9198364aad
Abhijana <abhijanaramanda@gmail.com>
2024-04-20 18:44:53 +0800

    feat: proof of existence functions

--- src/proof_of_existence.rs
+++ src/proof_of_existence.rs
@@ -1,6 +1,8 @@
 use core::fmt::Debug;
 use std::collections::BTreeMap;
 
+use crate::support::DispatchResult;
+
 pub trait Config: crate::system::Config {
 	/// The type which represents the content that can be claimed using this pallet.
 	/// Could be the content directly as bytes, or better yet the hash of that content.
@@ -21,5 +23,62 @@ impl<T: Config> Pallet<T> {
 	/// Create a new instance of the Proof of Existence Module.
 	pub fn new() -> Self {
 		Pallet { claims: BTreeMap::new() }
+	}
+
+	/// Get the owner (if any) of a claim.
+	pub fn get_claim(&self, claim: &T::Content) -> Option<&T::AccountId> {
+		self.claims.get(claim)
+	}
+
+	/// Create a new claim on behalf of the `caller`.
+	/// This function will return an error if someone already has claimed that content.
+	pub fn create_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
+		if let Some(_) = self.get_claim(&claim) {
+			return Err("claim already exist");
+		};
+		self.claims.insert(claim, caller);
+		Ok(())
+	}
+
+	/// Revoke an existing claim on some content.
+	/// This function should only succeed if the caller is the owner of an existing claim.
+	/// It will return an error if the claim does not exist, or if the caller is not the owner.
+	pub fn revoke_claim(&mut self, caller: T::AccountId, claim: T::Content) -> DispatchResult {
+		match self.get_claim(&claim) {
+			Some(owner) => {
+				if *owner != caller {
+					return Err("Caller is not the owner");
+				}
+			},
+			None => return Err("claim does not exist"),
+		}
+		self.claims.remove(&claim);
+		Ok(())
+	}
+}
+
+#[cfg(test)]
+mod test {
+	struct TestConfig;
+
+	impl super::Config for TestConfig {
+		type Content = &'static str;
+	}
+
+	impl crate::system::Config for TestConfig {
+		type AccountId = &'static str;
+		type BlockNumber = u32;
+		type Nonce = u32;
+	}
+
+	#[test]
+	fn basic_proof_of_existence() {
+		/*
+			TODO:
+			Create an end to end test verifying the basic functionality of this pallet.
+				- Check the initial state is as you expect.
+				- Check that all functions work successfully.
+				- Check that all error conditions error as expected.
+		*/
 	}
 }
